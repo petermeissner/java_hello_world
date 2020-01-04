1f63fe26d12aa2483d655c7dd0f8b06b4451c439
petermeissner <retep.meissner@gmail.com>
2020-01-04 23:00:04 +0100

    get rid of multi arch isntallation check errors the brute force way

--- R/zzz.r
+++ R/zzz.r
@@ -1,8 +1,13 @@
 .onLoad <- function(libname, pkgname) {
+
+  # let rJava find right Java version on its own
+  Sys.setenv(JAVA_HOME = "")
+
   rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
   rJava::.jaddClassPath(
     list.files(
       system.file("java", package = "javahelloworld")
     )
   )
+
 }
