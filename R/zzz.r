3e359b1f936c0468ce50977eb148932090e25414
petermeissner <retep.meissner@gmail.com>
2020-01-05 15:59:28 +0100

    experiment with rJava multi-arch installation check problems

--- R/zzz.r
+++ R/zzz.r
@@ -1,7 +1,17 @@
 .onLoad <- function(libname, pkgname) {
 
   # let rJava find right Java version on its own
-  Sys.setenv(JAVA_HOME = "")
+  if ( grepl( "^i386", utils::sessionInfo()$platform) ){
+    Sys.setenv(
+      JAVA_HOME =
+        tail(
+          list.files("C:/Program Files (x86)/Java", full.names = TRUE),
+          1
+        )
+    )
+  } else {
+    Sys.setenv(JAVA_HOME = "")
+  }
 
   rJava::.jpackage(pkgname, jars = "*", lib.loc = libname)
   rJava::.jaddClassPath(
